e8594472ddc3554de869b575ac55ed60fe09ca9c
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-01-28 17:32:59 +0700

    textscan không đọc được các dữ liệu của 2 dòng đầu tiên

--- dependencies/matlab_graph_utils/utils/load_graph.m
+++ dependencies/matlab_graph_utils/utils/load_graph.m
@@ -1,4 +1,4 @@
-%% 
+%%
 % Copyright (c) 2017 Carnegie Mellon University, Sanjiban Choudhury <sanjibac@andrew.cmu.edu>
 %
 % For License information please see the LICENSE file in the root directory.
@@ -9,14 +9,15 @@ function G = load_graph( filename )
 %   Detailed explanation goes here
 
 fid = fopen(filename, 'r');
-num_vertices = textscan(fid, 'NumVertices: %d');
-num_edges = textscan(fid, 'NumEdges: %d');
-G = sparse(double(num_vertices{1}), double(num_vertices{1}));
+line = fgetl(fid);  % Đọc một dòng từ tệp
+num_vertices = sscanf(line, 'NumVertices: %d %d')
+line = fgetl(fid);
+[num_edges] = sscanf(line, 'NumEdges: %d');
+G = sparse(double(num_vertices(1)), double(num_vertices(1)));
 raw_graph = textscan(fid,'%d %d %d %f\n');
-for i = 1:double(num_edges{1})
+for i = 1:double(num_edges(1))
     G(double(raw_graph{2}(i)), double(raw_graph{3}(i))) = raw_graph{4}(i);
 end
 fclose(fid);
 
 end
-

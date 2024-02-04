b330af7b9fa140b63360fe645674d5418c733790
bkhnk48 <48162469+bkhnk48@users.noreply.github.com>
2024-02-04 12:03:03 +0700

    giữ lại lớp abstract nhưng pt ko abstract
    gỡ đi lớp matlab.mixin.Copyable vì nó không có trong Octave

--- collision_checking_policies/CollisionCheckPolicy.m
+++ collision_checking_policies/CollisionCheckPolicy.m
@@ -1,29 +1,58 @@
-%% 
-% Copyright (c) 2017 Carnegie Mellon University, Sanjiban Choudhury <sanjibac@andrew.cmu.edu>
+%%
+% This file is part of the mat2cpp project.
+% mat2cpp is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
 %
-% For License information please see the LICENSE file in the root directory.
+% mat2cpp is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+%
+% You should have received a copy of the GNU General Public License
+% along with mat2cpp; see the file COPYING.  If not, write to
+% the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+% Boston, MA 02110-1301, USA.
 %
 
-classdef (Abstract) CollisionCheckPolicy < handle & matlab.mixin.Copyable
+classdef (Abstract) CollisionCheckPolicy %< handle & matlab.mixin.Copyable
     %COLLISIONCHECKPOLICY An interface class for collision checking
     %contracts
 
     properties
     end
-    
-    methods (Abstract)
-        edgeid = getEdgeToCheck(self); 
-        % chooses the next action
-        
-        setOutcome(self, selected_edge, outcome); 
-        % update after new observation
-        
-        plotDebug2D(self, graph, coord_set, path_library);
-        % plot stuff
-        
-        printDebug(self);
-        % Print anything for debugging
-    end
-    
-end
 
+    %methods (Abstract)
+    %    edgeid = getEdgeToCheck(self);
+        % chooses the next action
+
+    %    setOutcome(self, selected_edge, outcome);
+        % update after new observation
+
+    %   plotDebug2D(self, graph, coord_set, path_library);
+        % plot stuff
+
+    %   printDebug(self);
+        % Print anything for debugging
+    %end
+
+    methods
+        function edgeid = getEdgeToCheck(self)
+        % code here
+        end
+
+        function setOutcome(self, selected_edge, outcome)
+        % code here
+        end
+
+        function plotDebug2D(self, graph, coord_set, path_library)
+        % code here
+        end
+
+        function printDebug(self)
+        % code here
+        end
+    end
+
+end
